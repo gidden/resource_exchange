d0cbe932755ee6aaa6bb7ea4caea4e27910d0534
Matthew Gidden <matthew.gidden@gmail.com>
2013-01-25 15:00:57 -0600

    added comments

--- src/resources/isotope.h
+++ src/resources/isotope.h
@@ -7,14 +7,19 @@ namespace resource
   class isotope
   {
   public:
+    /// constructor, defaults to hydrogen
     isotope(int A=1, int Z=1, bool meta=false);
 
+    /// @return the atomic number
     int A();
     
+    /// @return the proton number
     int Z();
 
+    /// @return the meta stable state flag
     bool meta();
     
+    /// @return the ZAID number
     int zaid();
 
   private:
